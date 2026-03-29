b0b5b2cbd4661f09a0092d25696f20d9c82fb47b
Dimple-Saini06 <dimple13619saini@gmail.com>
2026-03-29 07:23:23 +0530

    Day-08

---
+++ Day-07-08/src/TodoList.jsx
@@ -0,0 +1,71 @@
+import {useState} from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+export default function TodoList() {
+    let[todos, setTodos] = useState([{task:"sample Task", id:uuidv4()}]);
+    let[newTodo, setNewTodo] = useState("");
+
+    function addNewTask() {
+        setTodos([...todos, {task:newTodo, id:uuidv4()}]);
+        setNewTodo("");
+    }
+
+    function updateTodoList(event) {
+        setNewTodo(event.target.value);
+    }
+
+    function deleteTodo(id){
+        setTodos((prevTodo) => todos.filter((prevTodo) => prevTodo.id != id));
+    }
+
+    function updateAll(){
+        setTodos(
+            todos.map((todo)=>{
+                return {
+                    ...todo,
+                    task : todo.task.toUpperCase(),
+                };
+            })
+        )
+    }
+
+    function upperCaseOne(id){
+        setTodos(
+            todos.map((todo)=>{
+                if(todo.id == id){
+                    return {
+                        ...todo,
+                        task : todo.task.toUpperCase(),
+                    }
+                }else{
+                    return todo;
+                }
+            })
+        )
+    }
+    return(
+        <div>
+            
+            <input placeholder="Enter Task" value={newTodo} onChange={updateTodoList} />
+            &nbsp;
+            <button onClick={addNewTask}>Add Task</button>
+            <hr></hr>
+            <h4>Todo List</h4>
+            <ul>
+                {
+                    todos.map((todo)=>(
+                        <li key={todo.id}>
+                            <span>{todo.task}</span>
+                            &nbsp;
+                            <button onClick={()=>deleteTodo(todo.id)}>Delete</button>
+                            &nbsp;
+                            <button onClick={()=>upperCaseOne(todo.id)}>UpperCase One</button>
+                        </li>
+                    ))
+                }
+            </ul>
+            <button onClick={updateAll}>update All</button>
+        </div>
+        
+    )
+}
