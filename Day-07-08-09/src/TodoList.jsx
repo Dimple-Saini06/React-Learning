23d1a5c20e101c1d234747eb6163dbe08f341e5f
Dimple-Saini06 <dimple13619saini@gmail.com>
2026-04-02 19:06:25 +0530

    mini change

--- Day-07-08-09/src/TodoList.jsx
+++ Day-07-08-09/src/TodoList.jsx
@@ -6,7 +6,7 @@ export default function TodoList() {
     let[newTodo, setNewTodo] = useState("");
 
     function addNewTask() {
-        setTodos([...todos, {task:newTodo, id:uuidv4(), isDone : false}]);+
+        setTodos([...todos, {task:newTodo, id:uuidv4(), isDone : false}]);
         setNewTodo("");
     }
 
