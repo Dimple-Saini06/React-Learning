00cb89de6ec41b1b71cad5f0a5c14e9201a85e5e
Dimple-Saini06 <dimple13619saini@gmail.com>
2026-04-03 09:46:18 +0530

    Learn design pattern as Lifting state up topic..

--- Day-10-11-12/src/App.jsx
+++ Day-10-11-12/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import Lottery from './Lottery.jsx'
+import Ticket from './Ticket.jsx'
 
 function App() {
 
   return (
     <>
-      <Lottery />
+      <Lottery n={3} winningSum={15}/>
     </>
   )
 }
--- Day-10-11-12/src/Lottery.jsx
+++ Day-10-11-12/src/Lottery.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
 import './Lottery.css'
 import { genTicket, sum } from './helper.js'
+import Ticket from './Ticket.jsx'
 
-export default function Lottery() {
+export default function Lottery({n=3, winningSum=15}) {
     // let [random, setRandom] = useState(Math.floor(Math.random() * 900) + 100);
     // let [result, setResult] = useState("Lottery");
 
@@ -40,22 +41,19 @@ export default function Lottery() {
         // </div>
     // )
     
-    let [ticket, setTicket] = useState(genTicket(3));
+    // let [ticket, setTicket] = useState(genTicket(3));
+    // let isWinning = sum(ticket) === 15;
 
-    let isWinning = sum(ticket) === 15;
-    
+    let [ticket, setTicket] = useState(genTicket(n));
+    let isWinning = sum(ticket) === winningSum;
+
     let buyTicket = () => {
-        setTicket(genTicket(3));
+        setTicket(genTicket(n));
     }
     return(
         <div>
             <h1>Lottery Game!</h1>
-            <div className="ticket">
-                <span>{ticket[0]}</span>
-                <span>{ticket[1]}</span>
-                <span>{ticket[2]}</span>
-            </div>
-            <br></br>
+            <Ticket ticket={ticket}/>
             <button onClick={buyTicket}>Buy New Ticket</button>
             
             <h3>{isWinning && "Congratulations, you won!"}</h3>
