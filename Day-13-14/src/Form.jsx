6e3eaf16f165fe634a93a24c1f2748122fe7cc7f
Dimple-Saini06 <dimple13619saini@gmail.com>
2026-04-11 08:46:05 +0530

    Day-14

--- Day-13-14/src/Form.jsx
+++ Day-13-14/src/Form.jsx
@@ -9,7 +9,8 @@ export default function Form(){
 
     return(
         <div>
-            <input placeholder="Enter full name" type="text" value={fullName} onChange={handleName} />
+            <label htmlFor='userName'>Full Name</label>
+            <input placeholder="Enter full name" type="text" value={fullName} onChange={handleName} id='userName' />
             <button>Submit</button>
         </div>
     )
