0cf23f770dde290df272a29bcec03fae9a8a408e
Dimple-Saini06 <dimple13619saini@gmail.com>
2026-04-11 12:58:48 +0530

    Day-14

--- Day-13-14/src/Form.jsx
+++ Day-13-14/src/Form.jsx
@@ -1,17 +1,43 @@
 import { useState } from 'react'
 
 export default function Form(){
-    let [fullName, setFullName] = useState("");
+    // let [fullName, setFullName] = useState("");
 
-    function handleName(event) {
-        setFullName(event.target.value);
+    // function handleName(event) {
+    //     setFullName(event.target.value);
+    // }
+
+    let[formData, setFormData] = useState({
+        fullName : "",
+        userName : ""
+    })
+
+    function handleData(event) {
+        setFormData(() => {
+            return{
+                ...formData, [event.target.name] : event.target.value 
+            }
+        })
+        
     }
 
+    const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form Submitted:", formData);
+    setFormData({
+        fullName : "",
+        userName : ""
+    })
+  };
     return(
-        <div>
-            <label htmlFor='userName'>Full Name</label>
-            <input placeholder="Enter full name" type="text" value={fullName} onChange={handleName} id='userName' />
+        <form onSubmit={handleSubmit}>
+            <label htmlFor='fullName'>Full Name</label>
+            <input placeholder="Enter full name" type="text" value={formData.fullName} onChange={handleData} id='fullName' name="fullName" />
+            <br></br>
+            <label htmlFor='userName'>User Name</label>
+            <input placeholder="Enter User name" type="text" value={formData.userName} onChange={handleData} id='userName' name="userName" />
+            <br></br>
             <button>Submit</button>
-        </div>
+        </form>
     )
 }
