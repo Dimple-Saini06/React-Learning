7388a5cb06383eb57d30e9662bd37d9007309ae3
Dimple-Saini06 <dimple13619saini@gmail.com>
2026-04-14 20:01:27 +0530

    Day-16

--- Day-13-14-15-16/src/CommentsForm.jsx
+++ Day-13-14-15-16/src/CommentsForm.jsx
@@ -1,73 +1,73 @@
 import { useState } from 'react'
-import { useFormik } from 'formik';
+// import { useFormik } from 'formik';
 
-const validate = values => {
-   const errors = {};
-   if (!values.username) {
-     errors.username = 'Required';
-   } 
-    return errors;
-};
+// const validate = values => {
+//    const errors = {};
+//    if (!values.username) {
+//      errors.username = 'Required';
+//    } 
+//     return errors;
+// };
 
 export default function CommentsForm({addNewComment}) {
-    // let[formData, setFormData] = useState({
-    //     username : "",
-    //     remarks : "",
-    //     rating : ""
-    // })
+    let[formData, setFormData] = useState({
+        username : "",
+        remarks : "",
+        rating : ""
+    })
 
-    // function handleData(event) {
-    //     setFormData(() => {
-    //         return{
-    //             ...formData, [event.target.name] : event.target.value
-    //         } 
-    //     })
-    // }
+    function handleData(event) {
+        setFormData(() => {
+            return{
+                ...formData, [event.target.name] : event.target.value
+            } 
+        })
+    }
 
-    // function defaultRemove(event){
-    //     event.preventDefault();
-    //     console.log("form is Submitted : ", formData)
-    //     addNewComment(formData);
-    //     setFormData({
-    //         username : "",
-    //         remarks : "",
-    //         rating : ""
-    //     })
-    // }
+    function defaultRemove(event){
+        event.preventDefault();
+        console.log("form is Submitted : ", formData)
+        addNewComment(formData);
+        setFormData({
+            username : "",
+            remarks : "",
+            rating : ""
+        })
+    }
 
-    const formik = useFormik({
-     initialValues: {
-       username: '',
-       remarks: '',
-       rating: 5,
-     },
-     validate,
-     onSubmit: values => {
-       alert(JSON.stringify(values, null, 2));
-     },
-   });
+//     const formik = useFormik({
+//      initialValues: {
+//        username: '',
+//        remarks: '',
+//        rating: 5,
+//      },
+//      validate,
+//      onSubmit: values => {
+//        alert(JSON.stringify(values, null, 2));
+//      },
+//    });
 
     return(
         <div>
             <h4>Give a Comment!</h4>
-            <form onSubmit={formik.handleSubmit}>
+            <form onSubmit={defaultRemove}>
                 <label htmlFor='username'>UserName</label>
                 &nbsp;
                 <input placeholder='username' 
-                    value={formik.values.username} 
-                    onChange={formik.handleChange}
+                    value={formData.username} 
+                    onChange={handleData}
                     name="username" 
                     id='username' >
                 </input>
-                {formik.errors.username ? <div style={{color:"red"}}>{formik.errors.username}</div> : null}
+                {/* {formik.errors.username ? <div style={{color:"red"}}>{formik.errors.username}</div> : null} */}
                 <br></br><br></br>
 
                 <label htmlFor='rating'>Rating</label>
                 &nbsp;
                 <input placeholder='rating' 
                     type='number' min={1} max={5} 
-                    value={formik.values.rating} 
-                    onChange={formik.handleChange} 
+                    value={formData.rating} 
+                    onChange={handleData} 
                     name="rating" 
                     id='rating' >
                 </input>
@@ -76,8 +76,8 @@ export default function CommentsForm({addNewComment}) {
 
                 <label htmlFor='remark'>Remarks</label>
                 &nbsp;
-                <textarea placeholder="add few remarks" value={formik.values.remarks} 
-                    onChange={formik.handleChange} 
+                <textarea placeholder="add few remarks" value={formData.remarks} 
+                    onChange={handleData} 
                     name="remarks" 
                     id='remark' >
                 </textarea>
